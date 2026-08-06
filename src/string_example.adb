707ff42ed8a86bf1c741c7a929d37cf28e035232
Thomas CIMINO <tci03@vm-build>
2026-08-06 20:55:16 +0200

    Add Get_Name_Unbounded in string_example.adb.

--- src/string_example.adb
+++ src/string_example.adb
@@ -1,12 +1,31 @@
 with Ada.Text_IO;
+with Ada.Strings.Unbounded;
 
 package body String_Example is
-	procedure Get_Name is
-	Answer : String (1 .. 20);
-	Last   : Integer;
+    --  User enter name and procedure outputs name and number of character of it:
+    --  Fixed strings version:
+	
+    procedure Get_Name is
+	    Answer : String (1 .. 20);
+	    Last   : Integer;
 	begin
-		Ada.Text_IO.Put ("What is your name ?");
+		Ada.Text_IO.Put ("What is your name ? ");
 		Ada.Text_IO.Get_Line (Answer, Last);
 		Ada.Text_IO.Put_line ("Your name is" & Answer (1 .. Last) & " and contains" & Integer'Image(Last) & " characters.");
 	end Get_Name;
+    
+    --  User enter name and procedure outputs name and number of character of it:
+    --  Unbounded strings version:
+    
+    procedure Get_Name_Unbounded is
+        Answer_U : Ada.Strings.Unbounded.Unbounded_String;
+    begin
+        Ada.Text_IO.Put ("What is your name ? ");
+        Answer_U := Ada.Strings.Unbounded.To_Unbounded_String (Ada.Text_IO.Get_Line);
+        Ada.Text_IO.Put_Line ("Your name is " & 
+            Ada.Strings.Unbounded.To_String (Answer_U) & 
+            " and contains" & 
+            Integer'Image (Ada.Strings.Unbounded.Length (Answer_U)) & 
+            " characters.");
+    end Get_Name_Unbounded;
 end String_Example;
--- src/string_example.ads
+++ src/string_example.ads
@@ -1,3 +1,4 @@
 package String_Example is
-   procedure Get_Name;
+    procedure Get_Name;
+    procedure Get_Name_Unbounded;
 end String_Example;
